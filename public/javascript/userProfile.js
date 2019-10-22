18ca76cc06cd4382e1bc6d0c62e1fafe8040163d
NR5P <orangev8zcar@gmail.com>
2019-10-22 18:29:04 -0500

    phone number auto dashes now works

--- public/javascript/userProfile.js
+++ public/javascript/userProfile.js
@@ -2,7 +2,7 @@ const phoneNumber = document.getElementById("phoneNumber");
 
 phoneNumber.addEventListener("keyup", (e)=> {
     if ((e.target.value.length === 3 || e.target.value.length === 7)
-        && (e.keyCode !== 8 || e.keyCode !== 46)) {
+        && (e.keyCode !== 8 && e.keyCode !== 46)) {
         phoneNumber.value += "-";
     }
 })
